97104f774aa94dd8f6c5f486a9399a7f6c3dbb15
Nitin Gautam <nitingautam15@gmail.com>
2020-09-05 02:04:51 +0530

    1409B Codefocrces
    1409B Codefocrces

---
+++ codeforces1409B.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long res(long long a,long long b,long long x,long long y,long long n){
+    long long t = min(a-x,n);
+    a -=t;
+    n -=t;
+    t = min(b-y,n);
+    b -=t;
+    
+    return a*b;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    long long a,b,x,y,n;
+    cin>>a>>b>>x>>y>>n;
+    cout<<min(res(a,b,x,y,n),res(b,a,y,x,n));
+    }
+
+
+    return 0;
+}
